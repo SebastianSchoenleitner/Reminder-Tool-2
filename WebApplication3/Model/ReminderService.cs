e13629df1bf44f890b432b2dc49b537f5f293ea8
Schoenleitner_Sebastian <schoenleitners180118@sus.htl-grieskirchen.at>
2024-01-08 10:25:22 +0100

    ReminderServiceAuskommentiert

--- WebApplication3/Model/ReminderService.cs
+++ WebApplication3/Model/ReminderService.cs
@@ -4,16 +4,16 @@ namespace WebApplication3.Model
 {
     public class ReminderService
     {
-        private readonly ReminderContext _dbContext;
-        public ReminderService(ReminderContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
+        //private readonly ReminderContext _dbContext;
+        //public ReminderService(ReminderContext dbContext)
+        //{
+        //    _dbContext = dbContext;
+        //}
 
-        public void AddReminder(Reminder newReminder)
-        {
-            _dbContext.Reminders.Add(newReminder);
-            _dbContext.SaveChanges();
-        }
+        //public void AddReminder(Reminder newReminder)
+        //{
+        //    _dbContext.Reminders.Add(newReminder);
+        //    _dbContext.SaveChanges();
+        //}
     }
 }

57824039b134c56daeb7ac9ca009b600a0a89e81
Schoenleitner_Sebastian <schoenleitners180118@sus.htl-grieskirchen.at>
2024-01-08 10:18:50 +0100

    commit_Sebi

---
+++ WebApplication3/Model/ReminderService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication3.Model
+{
+    public class ReminderService
+    {
+        private readonly ReminderContext _dbContext;
+        public ReminderService(ReminderContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void AddReminder(Reminder newReminder)
+        {
+            _dbContext.Reminders.Add(newReminder);
+            _dbContext.SaveChanges();
+        }
+    }
+}
